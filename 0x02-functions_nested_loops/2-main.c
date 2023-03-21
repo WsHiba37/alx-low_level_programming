3b4cdb13320bdd80c6e0a925ff554fc1f2fd2442
Hiba Ouaddad <houaddad@gmail.com>
2023-03-21 15:36:41 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
