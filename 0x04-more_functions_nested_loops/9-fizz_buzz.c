c4cd75118fea91dda793dc8d4186007bff0a1415
Hiba Ouaddad <houaddad@gmail.com>
2023-03-23 14:14:18 -0700

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,25 @@ int main(void)
 	{
 		if (x % 5 == 0 && x % 3 == 0)
 	{
-		printf("FizzBuzz");
+		printf(" FizzBuzz");
 	}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
-		else
+		else if (x == 1)
 		{
 			printf("%d", x);
 		}
-		putchar(' ');
+		else
+		{
+			printf(" %d", x);
+		}
+
 	}
 	putchar('\n');
 	return (0);
