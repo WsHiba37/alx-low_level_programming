bfb6ef6e43c4fefda571eb42e87e7437798fae87
Hiba Ouaddad <houaddad@gmail.com>
2023-03-23 10:48:45 -0700

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - The Fizz-Buzz test
+ *
+ * @x
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 5 == 0 && x % 3 == 0)
+	{
+		printf("FizzBuzz");
+	}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
