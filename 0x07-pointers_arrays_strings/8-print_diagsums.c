89a460a42e4a2020b6a0f87ce01a19bcd44bf998
Hiba Ouaddad <houaddad@gmail.com>
2023-04-05 14:58:18 +0000

    Write a function that prints a string, followed by a new line.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-/**
- * print_diagsums - a function that prints the sum of the two diagonals of a suare matrix of integers
- * @a: pointer to the matrix array
- * @size: size of the array
- * Return: void
- */
